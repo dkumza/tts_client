b88cad1c5ae32db282adc73a20be42743bc755f2
Darius <dariuskumza@gmail.com>
2024-01-14 02:20:29 +0200

    Refactor SingleProduct component layout

--- src/components/pages/products/SingleProduct.jsx
+++ src/components/pages/products/SingleProduct.jsx
@@ -11,66 +11,62 @@ export const SingleProduct = ({ ad }) => {
       );
    }
    return (
-      <Link to={`/product/${ad.id}`}>
-         <li className="bg-stone-100 hover:bg-gray-50 rounded text-md min-w-[970px]  min-h-[184px] flex shadow hover:cursor-pointer">
-            <div className="div flex min-w-full  gap-1 ">
-               <div className="flex min-w-full flex-1 h-full">
-                  <div className="img flex flex-col justify-between  h-full border">
-                     <img
-                        className="border h-full min-w-[352px]  border-red-200"
-                        src=""
-                        alt={ad.title}
-                     />
+      <li className="bg-stone-100 hover:bg-gray-50 rounded text-md min-w-[970px]  min-h-[184px] flex shadow">
+         <div className="div flex min-w-full  gap-1 ">
+            <div className="flex min-w-full flex-1 h-full">
+               <div className="img flex flex-col justify-between  h-full border">
+                  <img
+                     className="border h-full min-w-[352px]  border-red-200"
+                     src=""
+                     alt={ad.title}
+                  />
+               </div>
+               <div className="flex w-full flex-col justify-between align-middle items-start p-6">
+                  <div className="title flex flex-col gap-2 w-full">
+                     <div className="title text-xs flex gap-2 items-center justify-between w-full">
+                        <div className="flex gap-2 items-center w-full">
+                           <div className="bg-amber-400 rounded text-black px-2 py-1 capitalize">
+                              {ad.p_condition}
+                           </div>
+                           <Link
+                              to={`/products/category/${ad.cat_id}`}
+                              className="bg-sky-300 px-2 py-1 text-black rounded"
+                           >
+                              {/* {ad.cat_id} */} Desktop Pcs
+                           </Link>
+                           <div
+                              className={`${
+                                 dateDay > 30 ? 'bg-amber-400' : 'bg-lime-400'
+                              } px-2 py-1 rounded`}
+                           >
+                              <span className="font-medium">
+                                 {dateDay === 0
+                                    ? 'Listed: Today'
+                                    : `Listed: ${dateDay} days ago`}
+                              </span>
+                           </div>
+                        </div>
+                        <div className="w-full text-right">
+                           by {ad.username}
+                        </div>
+                     </div>
+                     <div className="title flex flex-col gap-2">
+                        <h1 className="text-md font-semibold">{ad.title}</h1>
+                        <p className="text-sm">{ad.content}</p>
+                     </div>
                   </div>
-                  <div className="flex w-full flex-col justify-between align-middle items-start p-6">
-                     <div className="title flex flex-col gap-2 w-full">
-                        <div className="title text-xs flex gap-2 items-center justify-between w-full">
-                           <div className="flex gap-2 items-center w-full">
-                              <div className="bg-amber-400 rounded text-black px-2 py-1 capitalize">
-                                 {ad.p_condition}
-                              </div>
-                              <Link
-                                 to={`/products/category/${ad.cat_id}`}
-                                 className="bg-sky-300 px-2 py-1 text-black rounded"
-                              >
-                                 {/* {ad.cat_id} */} Desktop Pcs
-                              </Link>
-                              <div
-                                 className={`${
-                                    dateDay > 30
-                                       ? 'bg-amber-400'
-                                       : 'bg-lime-400'
-                                 } px-2 py-1 rounded`}
-                              >
-                                 <span className="font-medium">
-                                    {dateDay === 0
-                                       ? 'Listed: Today'
-                                       : `Listed: ${dateDay} days ago`}
-                                 </span>
-                              </div>
-                           </div>
-                           <div className="w-full text-right">
-                              by {ad.username}
-                           </div>
-                        </div>
-                        <div className="title flex flex-col gap-2">
-                           <h1 className="text-md font-semibold">{ad.title}</h1>
-                           <p className="text-sm">{ad.content}</p>
-                        </div>
-                     </div>
-                     <div className="flex items-center justify-between w-full">
-                        <div className="text-2xl font-semibold">
-                           {' '}
-                           € {ad.price}
-                        </div>
-                        <div className="py-1 bg-sky-500 px-6 rounded text-white">
-                           More
-                        </div>
-                     </div>
+                  <div className="flex items-center justify-between w-full">
+                     <div className="text-2xl font-semibold"> € {ad.price}</div>
+                     <Link
+                        to={`/product/${ad.id}`}
+                        className="py-1 bg-sky-400 px-6 rounded text-white font-semibold"
+                     >
+                        More
+                     </Link>
                   </div>
                </div>
             </div>
-         </li>
-      </Link>
+         </div>
+      </li>
    );
 };
--- src/components/pages/products/SingleProductPage.jsx
+++ src/components/pages/products/SingleProductPage.jsx
@@ -42,7 +42,7 @@ export const SingleProductPage = () => {
 
    return (
       <div className="container mx-auto min-h-full my-8">
-         <div className="py-4  flex align-middle items-center">
+         {/* <div className="py-4  flex align-middle items-center">
             <Link
                className=" hover:cursor-pointer text-gray-700 hover:text-black bg-white pl-2 flex items-center gap-1"
                to={'/'}
@@ -70,11 +70,11 @@ export const SingleProductPage = () => {
                   <p> To cat {productFromAPI.cat_id}</p>
                </Link>
             )}
-         </div>
+         </div> */}
          {!productFromAPI && <p>Loading</p>}
          <div className="min-h-full">
             {productFromAPI && (
-               <div className="div flex md:flex-row flex-col gap-6  min-h-full">
+               <div className="div flex md:flex-row flex-col gap-6  min-h-full border ">
                   <div className="left bg-white md:w-6/12 min-h-full flex flex-col">
                      <div className="h-full ">
                         <img
@@ -88,34 +88,46 @@ export const SingleProductPage = () => {
                         <div className="price text-3xl font-semibold py-2 text-gray-800">
                            € {productFromAPI.price}
                         </div>
-                        <div className="price text-xl font-semibold px-6 py-2 bg-amber-400 hover:bg-sky-600 hover:cursor-pointer hover:text-amber-400">
+                        <div className="price text-xl font-semibold px-6 py-2 rounded bg-amber-400 hover:bg-sky-600 hover:cursor-pointer hover:text-amber-400">
                            BUY
                         </div>
                      </div>
                   </div>
-                  <div className="right w-full border-b-4 border-white flex flex-col gap-4 relative justify-start">
+                  <div className="right w-full flex flex-col gap-4 justify-between  border pt-">
                      <div className="title flex flex-col gap-2 justify-start">
                         <h1 className="text-4xl font-semibold text-stone-700">
                            {productFromAPI.title}
                         </h1>
+                        <p className="py-1 text-xs">
+                           By{' '}
+                           <span className="bg-white ml-1 px-2 font-medium py-1 rounded">
+                              {productFromAPI.username}
+                           </span>
+                        </p>
 
-                        <div className="flex justify-between text-sm">
+                        <div className="flex justify-between text-sm pt-2">
                            <div className="flex gap-2 items-center">
                               <p
                                  className={`${
                                     productFromAPI.p_condition === 'used'
                                        ? 'bg-amber-400'
                                        : 'bg-lime-400'
-                                 } capitalize w-fit px-4 py-1`}
+                                 } capitalize w-fit px-4 py-1 rounded`}
                               >
                                  {productFromAPI.p_condition}
                               </p>
+                              <Link
+                                 to={`/products/category/${productFromAPI.cat_id}`}
+                                 className="bg-sky-300 px-2 py-1 text-black rounded"
+                              >
+                                 {/* {ad.cat_id} */} Desktop Pcs
+                              </Link>
                               <div
                                  className={`${
                                     dateDay > 30
                                        ? 'bg-amber-400'
                                        : 'bg-lime-400'
-                                 } px-2 py-1`}
+                                 } px-2 py-1 rounded`}
                               >
                                  <span className="font-medium">
                                     {dateDay === 0
@@ -123,38 +135,14 @@ export const SingleProductPage = () => {
                                        : `Listed: ${dateDay} days ago`}
                                  </span>
                               </div>
-                              <p className="py-1">
-                                 By{' '}
-                                 <span className="bg-white ml-1 px-2 font-medium py-1">
-                                    {productFromAPI.username}
-                                 </span>
-                              </p>
                            </div>
-                           {/* <div className="wishlist px-4 py-2 hover:text-amber-400 border font-semibold shadow border-amber-400 bg-white hover:cursor-pointer flex items-center gap-2">
-                              <div className="text-rose-400">
-                                 <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-heart-fill"
-                                    viewBox="0 0 16 16"
-                                 >
-                                    <path
-                                       fillRule="evenodd"
-                                       d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                                    />
-                                 </svg>
-                              </div>
-                              <p>Wishlist</p>
-                           </div> */}
                         </div>
                      </div>
-                     <div className="about justify-end ">
+                     <div className="">
                         <h1 className="font-semibold py-2">
                            Product information from seller
                         </h1>
-                        <p className="bg-white p-4 text-sm h-full">
+                        <p className="bg-white p-4 text-sm">
                            {/* {productFromAPI.content} */}
                            Lorem ipsum dolor sit amet consectetur adipisicing
                            elit. Molestias error adipisci possimus sequi itaque
