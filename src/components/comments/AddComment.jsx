6a62b02a0d8a2e53e111824265bd01f67176ba32
Darius <dariuskumza@gmail.com>
2024-01-16 12:11:31 +0200

    prep for adding new comments

---
+++ src/components/comments/AddComment.jsx
@@ -0,0 +1,10 @@
+export const AddComment = () => {
+  return (
+    <div className="flex md:p-20 items-center align-middle justify-center min-w-full min-h-full text-white">
+      <div className="flex flex-col rounded-lg items-center bg-stone-800 justify-center align-middle mx-auto w-full max-w-md  px-12 py-14 shadow-sm min-h-full ">
+        <h1 className="mb-4 text-2xl text-center">Comment</h1>
+        <form></form>
+      </div>
+    </div>
+  );
+};
--- src/components/comments/CommentsSection.jsx
+++ src/components/comments/CommentsSection.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useAPiData } from '../../hooks/useAPiData';
 import { SingleComment } from './SingleComment';
+import { Sell } from '../pages/sell/Sell';
+import { AddComment } from './AddComment';
 
 const COMM_URL = `http://localhost:3000/api/comments/product`;
 
@@ -8,13 +10,18 @@ export const CommentsSection = ({ productID }) => {
   const pathToComments = `${COMM_URL}/${productID}`;
 
   const [dataFromAPi, setDataFromAPi, apiError] = useAPiData(pathToComments);
-  dataFromAPi && console.log('comments from API: ', dataFromAPi);
 
   return (
     <>
       <div className="flex justify-between items-center">
+        {/* <div className="bg-white opacity-30 w-full absolute h-full right-0 bottom-0 inset-x-1/2 inset-y-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="flex absolute right-0 bottom-0 inset-x-1/2 inset-y-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          <AddComment />
+        </div> */}
         <h1 className="font-semibold py-2">Comments ({dataFromAPi.length})</h1>
-        <Link className="py-1 my-2 bg-sky-400 px-4 rounded">To comment</Link>
+        <Link className="py-1 my-2 bg-sky-400 px-4 rounded text-white font-semibold">
+          To comment
+        </Link>
       </div>
       <div className="bg-white p-4 flex flex-col gap-2">
         {dataFromAPi &&
